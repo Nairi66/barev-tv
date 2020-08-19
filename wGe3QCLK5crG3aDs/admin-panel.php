716fcdb4b4fc1e5ba48da94c0e66c6a9b695e2aa
Nairi66 <69626762+Nairi66@users.noreply.github.com>
2020-08-19 11:54:46 +0400

    commit

---
+++ wGe3QCLK5crG3aDs/admin-panel.php
@@ -0,0 +1,69 @@
+<?php 
+	include_once '../session.php';
+	include '../reg_log/db.php';
+	if ($_SESSION['username'] == 'q8XG!P7>aZ?zp=3') {
+		
+	}else {
+		header('location: /barevtv');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: /barevtv/reg_log/login.php");
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Barev </title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+	<?php include_once '../assets/header.php'; ?>
+	<h1>Insert Films</h1>
+	<center><form method="post" action="insert_films.php" enctype="multipart/form-data"> 
+		  <div class="form-group col-sm-7">
+		    <label for="title">Ընտրեք Վերնագիր։ </label>
+		    <input type="text" placeholder="Վերնագիր" name="title" class="form-control" id="title">
+		  </div>
+		  <div class="form-group first col-sm-7">
+		    <label for="desc">Նկարագրություն։ </label>
+		    <input type="text" placeholder="Կարճ նկարագրություն" name="desc" id="desc" class="form-control">
+		  </div>
+		  <div class="form-group first col-sm-7">
+		    <label for="desc">Վերբեռնվող ֆայլը։ </label>
+		    <input type="file" name="file" id="upload" class="form-control">
+		  </div>
+		  <hr>
+		<div class="form-group col-sm-7" id="last">
+		    <input type="submit" name="login" id="sub" class="col-sm-4 btn btn-success" value="Submit"></input>
+		</div>
+	</form>
+	</center>
+	<h1>Insert Serials or Anime</h1>
+	<center>
+		<form method="post" action="insert_serials.php" enctype="multipart/form-data"> 
+		  <div class="form-group col-sm-7">
+		    <label for="title">Ընտրեք Վերնագիր։ </label>
+		    <input type="text" placeholder="Վերնագիր" name="title" class="form-control" id="title">
+		  </div>
+		  <div class="form-group first col-sm-7">
+		    <label for="desc">Նկարագրություն։ </label>
+		    <input type="text" placeholder="Կարճ նկարագրություն" name="desc" id="desc" class="form-control">
+		  </div>
+		  <div class="form-group first col-sm-7">
+		    <label for="desc">Վերբեռնվող ֆայլը։ </label>
+		    <input type="file" name="file" id="upload" class="form-control">
+		  </div>
+		  <hr>
+		<div class="form-group col-sm-7" id="last">
+		    <input type="submit" name="login" id="sub" class="col-sm-4 btn btn-success" value="Submit"></input>
+		</div>
+	</form>
+	</center>
+	
+</body>
+</html>
---
+++ wGe3QCLK5crG3aDs/insert_films.php
@@ -0,0 +1,67 @@
+<?php
+	
+	function alert($text, $spec){
+		echo "<div class='form-group col-sm-7 ' style='position: absolute; z-index: 9;'><div id='alert' class='col-sm-5 alert alert-".$spec."'>$text</div></div>";
+	}
+
+	function checkFilms($title){
+		include '../reg_log/db.php';
+		
+		$query = "SELECT * FROM `uploaded_films` WHERE `film_title` = '$title'";
+		$result = mysqli_query($conn, $query);
+		if (mysqli_num_rows($result)>=1) {
+
+		}else{
+			return true;
+		}
+	}
+
+	
+
+	function insertFilm($title, $desc, $filename){
+		include '../reg_log/db.php';
+		$query = "INSERT INTO uploaded_films (film_id, film_title, film_description, filename, date_added) VALUES ('','$title', '$desc', '$filename', NOW())";
+		$result = mysqli_query($conn, $query);
+
+		if(strlen($conn->error) == 0){
+			alert('Ֆիլմը հաջողությամբ վերբեռնված է։','success');
+			return true;
+		}else{
+			alert('Վերբեռնելու ընթացքում տեղի է ունեցել սխալ, խնրում ենք նորից փորձել։', 'danger');
+
+		}
+	}
+
+	//Insert FIlm
+	function Insert() {
+
+		if (isset($_POST['login'])){
+			if (!empty($_POST['title']) && !empty($_POST['desc'])) {
+				include '../reg_log/db.php';
+
+
+				$name = $_FILES['file']['name'];
+				$tmp_name = $_FILES['file']['tmp_name'];
+				//from POST
+				$title = $_POST['title'];
+				$desc = $_POST['desc'];
+				$dest = '../uploaded_films/'.$name;
+				if (checkFilms($title)) {
+					insertFilm($title, $desc, $name);
+					move_uploaded_file($tmp_name, $dest);
+					return true;
+				}else{
+					// alert('Տվյալ ֆիլմը արդեն կա բազայում։', 'danger');		
+					echo 'fuck';
+				}
+
+			}else{
+
+				// alert('Խնդրում ենք լրացնել բոլոր դաշտերը։', 'danger');
+
+			}
+		}
+	}
+	if (Insert()) {
+		header('location: admin-panel.php');
+	}
